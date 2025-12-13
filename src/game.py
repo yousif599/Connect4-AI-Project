0f91610da876b925927c0e626015df9b6dd063ee
Youssef <y19772005@gmail.com>
2025-12-13 23:16:24 +0200

    update game logic

---
+++ src/game.py
@@ -0,0 +1,35 @@
+from board import ROWS, COLS, PLAYER, AI, EMPTY, get_valid_locations
+
+def winning_move(board, piece):
+    # Horizontal
+    for c in range(COLS - 3):
+        for r in range(ROWS):
+            if all(board[r][c+i] == piece for i in range(4)):
+                return True
+
+    # Vertical
+    for c in range(COLS):
+        for r in range(ROWS - 3):
+            if all(board[r+i][c] == piece for i in range(4)):
+                return True
+
+    # Positive diagonal
+    for c in range(COLS - 3):
+        for r in range(ROWS - 3):
+            if all(board[r+i][c+i] == piece for i in range(4)):
+                return True
+
+    # Negative diagonal
+    for c in range(COLS - 3):
+        for r in range(3, ROWS):
+            if all(board[r-i][c+i] == piece for i in range(4)):
+                return True
+
+    return False
+
+def is_terminal_node(board):
+    return (
+        winning_move(board, PLAYER) or
+        winning_move(board, AI) or
+        len(get_valid_locations(board)) == 0
+    )
